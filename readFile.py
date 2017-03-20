8821f0bd9b86d20c556b4241fb9c9a70e81d5d9b
Rahul Yamasani <raya4213@colorado.edu>
2017-03-20 17:30:38 -0600

    Add files via upload

--- readFile.py
+++ readFile.py
@@ -8,12 +8,12 @@ def readFile():
     #send reprog_begin
     write("reprog_begin\n")
     time.sleep(1)
-    with open('C:\Users\colab\Documents\GitHub\SelfProgramming\ItsHexFileFlash\hexFile.txt','r') as fob:
+    with open('C:\Users\colab\Documents\GitHub\ItsHexFileFlash\hexFile.txt','r') as fob:
         print "Starting the reprogramming stuff"
         for line in fob:
             dat = []
             line = line.strip(':')
-            if line[2:8] == "B80000":
+            if line[2:8] == "C20000":
                 line = '! ' + line[0:6]+line[8:]
                 write(line)
                 time.sleep(0.2)   # we need to add this delay for the receiver
